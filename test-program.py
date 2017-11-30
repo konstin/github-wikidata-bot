a25107d97044b6cb6422df366136e69b1b0bc0cf
$GIT_USER_NAME <$GIT_USER_EMAIL>
2017-11-30 21:21:37 +0100

    updated test-program.py

--- test-program.py
+++ test-program.py
@@ -6,11 +6,16 @@ class Test:
 		self.errorLog = []
 	def run(self):
 		if github_repo_to_api("https://github.com/hallon") != "https://api.github.com/repos/hallo":
-			self.errorLog.append("ERR: github_repo_to_api")
+			self.errorLog.append("ERR#001")
 	def printErrorLog(self):
+		Error1 = Error()
 		for i in self.errorLog:
-			print(i)
-
+			Error1.raiseError(i)
+class Error:
+	def __init__(self):
+		self.errors = {"ERR#001":"Error: github_repo_to_api gave an ivalid return value."}
+	def raiseError(self, error):
+		print(self.errors[error])
 if __name__ == "__main__":
 	Test1 = Test()
 	Test1.run()
