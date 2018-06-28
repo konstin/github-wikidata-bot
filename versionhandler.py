aec9ab4e09e1249524093ff1a74c2fb303933443
Michael F. Schönitzer <michael@schoenitzer.de>
2018-06-29 00:48:29 +0200

    Improve version parsing

--- versionhandler.py
+++ versionhandler.py
@@ -1,37 +1,51 @@
 import re
 
 
-def extract_version(string, name=""):
+def extract_version(string, name=None):
     if type(string) is not str:
         return None
-    name += " "
-    if string.startswith(name):
-        string = string[len(name):]
+    if name:
+        namere = re.compile(r"^" + re.escape(name) + r"[ -_]", re.IGNORECASE)
+        match = namere.match(string)
+        if match:
+            string = string[match.end():]
     STABLE = "stable"
     UNSTABLE = "unstable"
     exact = re.compile(r"[vV]?(\d{1,5}(\.\d{1,3})*)")
     stable = re.compile(r"(\s|^|v)\d{1,3}(\.\d{1,3})+(-\d\d?|[a-z])?(\s|$)")
-    pre = re.compile(r"(\s|^|v)(\d{1,3}(\.\d{1,3})+)[.-]?(alpha|beta|pre|rc|b)[.-]?\d*(\s|$)",
-                     re.IGNORECASE)
+    pre = re.compile(
+        r"(\s|^|v)(\d{1,3}(\.\d{1,3})+)[.-]?(alpha|beta|pre|rc|b|preview)[.-]?\d*(\s|$)",
+        re.IGNORECASE
+    )
+    explicitstable = re.compile(
+        r"(\s|^|v)(\d{1,3}(\.\d{1,3})+)(-stable)(\s|$)")
 
     match = exact.fullmatch(string)
     if match:
         return (STABLE, match.group(1))
 
-    match = list(stable.finditer(string))
-    if len(match) == 1:
-        return (STABLE, match[0].group(0).strip())
+    match_stable = list(stable.finditer(string))
+    match_pre = list(pre.finditer(string))
+    match_explicit = list(explicitstable.finditer(string))
+    if len(match_stable) + len(match_pre) + len(match_explicit) > 1:
+        return None
+
+    if match_stable:
+        return (STABLE, match_stable[0].group(0).strip())
 
     match = list(pre.finditer(string))
-    if len(match) == 1:
+    if match_pre:
         state = re.search(
             r"[^a-zA-Z](alpha|beta|rc|b)($|[^a-zA-Z])", string, re.IGNORECASE)
         if state:
             statestr = state.group(1).lower()
             if statestr == 'b':
                 statestr = 'beta'
-            return (statestr, match[0].group(0))
+            return (statestr, match_pre[0].group(0))
         else:
-            return (UNSTABLE, match[0].group(0))
+            return (UNSTABLE, match_pre[0].group(0))
+
+    if match_explicit:
+        return (STABLE, match_explicit[0].group(2).strip())
 
     return None
--- versionhandler_test.py
+++ versionhandler_test.py
@@ -11,7 +11,10 @@ def test_stable():
     assert extract_version("1.23f") == ("stable", "1.23f")
     assert extract_version("1.2-1") == ("stable", "1.2-1")
     assert extract_version("1.2-12") == ("stable", "1.2-12")
+    assert extract_version("0.23.5-stable") == ("stable", "0.23.5")
     assert extract_version("program 1.2", "program") == ("stable", "1.2")
+    assert extract_version("program-1.2", "program") == ("stable", "1.2")
+    assert extract_version("Program-1.2", "program") == ("stable", "1.2")
 
 
 def test_unstable():
@@ -19,6 +22,7 @@ def test_unstable():
     assert extract_version("program 1.4alpha", "program") == ("alpha", "1.4alpha")
     assert extract_version("1.3beta") == ("beta", "1.3beta")
     assert extract_version("1.3pre") == ("unstable", "1.3pre")
+    assert extract_version("1.3-preview1") == ("unstable", "1.3-preview1")
     assert extract_version("1.3rc") == ("rc", "1.3rc")
     assert extract_version("1.3rc1") == ("rc", "1.3rc1")
     assert extract_version("1.4Alpha") == ("alpha", "1.4Alpha")
@@ -34,6 +38,8 @@ def test_invalid():
     assert extract_version("foo") is None
     assert extract_version("foo1.3") is None
     assert extract_version("1.3bar") is None
+    assert extract_version("1.3beta1.4") is None
+    assert extract_version("1.3beta 1.4-stable") is None
     assert extract_version("foo1.3bar") is None
     assert extract_version("foo1.3", "foo") is None
     assert extract_version("foo1.3bar", "foo") is None
