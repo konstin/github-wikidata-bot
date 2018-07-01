a39989851d53c690eaa12e791e042017a27c78ef
Michael F. Schönitzer <michael@schoenitzer.de>
2018-07-02 00:45:45 +0200

    Improvements to versiontest-tool

--- tools.py
+++ tools.py
@@ -1,22 +1,27 @@
 import argparse
 import json
+from random import sample
 
-from main import Settings, query_projects, get_all_github_releases, analyse_release
+from main import (Settings, analyse_release, get_all_github_releases, logger,
+                  query_projects)
 
 
-def debug_version_handling(threshold: int = 50):
-    with open("config.json") as config:
-        github_oath_token = json.load(config)["github-oauth-token"]
-    Settings.cached_session.headers.update(
-        {"Authorization": "token " + github_oath_token}
-    )
-    projects = query_projects()[:threshold]
+def safe_sample(pupulation, size):
+    try:
+        return sample(pupulation, size)
+    except ValueError:
+        return pupulation
+
+
+def debug_version_handling(threshold: int = 50, size: int = 20):
+    logger.setLevel(40)
+    projects = safe_sample(query_projects(), threshold)
     for project in projects:
-        github_releases = get_all_github_releases(project["repo"])
+        github_releases = safe_sample(get_all_github_releases(project["repo"]), size)
         for github_release in github_releases:
             release = analyse_release(github_release, project["projectLabel"])
             print(
-                "{:10} | {:10} | {:15} | {:25} | {}".format(
+                "{:15} | {:10} | {:20} | {:25} | {}".format(
                     release["version"] if release else "---",
                     release["release_type"] if release else "---",
                     github_release["tag_name"],
@@ -29,6 +34,16 @@ def debug_version_handling(threshold: int = 50):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--threshold", default=50, type=int)
+    parser.add_argument("--maxsize", default=20, type=int)
+    parser.add_argument("--github-oauth-token")
     args = parser.parse_args()
 
-    debug_version_handling(args.threshold)
+    if args.github_oauth_token:
+        github_oath_token = args.github_oauth_token
+    else:
+        with open("config.json") as config:
+            github_oath_token = json.load(config)["github-oauth-token"]
+    Settings.cached_session.headers.update(
+        {"Authorization": "token " + github_oath_token}
+    )
+    debug_version_handling(args.threshold, args.maxsize)
