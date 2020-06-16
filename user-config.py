b15ba37c49d87853640ec6bebc2d0d6df193f5a2
konstin <konstin@mailbox.org>
2020-06-16 20:47:22 +0200

    Increase maxlag to fix #115
    Thanks to @Nudin for the fix.

--- user-config.py
+++ user-config.py
@@ -17,3 +17,14 @@ minthrottle = 1
 del json
 del username
 del config
+
+# See https://github.com/konstin/github-wikidata-bot/issues/115#issuecomment-644403350
+# Maxlag. Higher values are more aggressive in seeking access to the wiki.
+maxlag = 8
+
+# Maximum number of times to retry an API request before quitting.
+max_retries = 30
+# Minimum time to wait before resubmitting a failed API request.
+retry_wait = 5
+# Maximum time to wait before resubmitting a failed API request.
+retry_max = 360
